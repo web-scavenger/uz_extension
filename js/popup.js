e6c82d1dbea63d5f861aa07409f40749d4620596
web-scavenger <dimana1507@gmail.com>
2018-09-05 00:27:57 +0300

    add trains list. User can Open checked train

--- js/popup.js
+++ js/popup.js
@@ -10,34 +10,34 @@ $(function () {
     $('#from_input').keyup(function () {
         var from = $('#from_input').val();
         getLocationsList(from, '#from_list', 'from__select');
-        
+
     })
     $('#to_input').keyup(function () {
         var to = $('#to_input').val();
-        getLocationsList(to, '#to_list', 'to__select' );
-        
+        getLocationsList(to, '#to_list', 'to__select');
+
     })
 
-    $('#calendar').change(function(){
+    $('#calendar').change(function () {
         opt.date = this.value;
     })
 
-    $('#check_trains').click(function(){
-        if(opt.fromValue == null || opt.toValue == null || opt.date == null){
+    $('#check_trains').click(function () {
+        if (opt.fromValue == null || opt.toValue == null || opt.date == null) {
             alert('Введите все данные')
         } else {
             getTrainsList();
         }
     })
 
-    $(document).on("click", ".from__select", function(e) {
+    $(document).on("click", ".from__select", function (e) {
         e.preventDefault();
         opt.fromValue = this.getAttribute('data-train');
         $('#from_input').val(this.getAttribute('data-location'))
-        $('#from_list').html('') 
+        $('#from_list').html('')
     });
 
-    $(document).on("click", ".to__select", function(e) {
+    $(document).on("click", ".to__select", function (e) {
         e.preventDefault();
         opt.toValue = this.getAttribute('data-train');
         $('#to_input').val(this.getAttribute('data-location'))
@@ -45,7 +45,7 @@ $(function () {
     });
 
 
-    getLocationsList = function(where, outputBlock, liClass){
+    getLocationsList = function (where, outputBlock, liClass) {
         $.ajax({
             type: 'GET',
             headers: {
@@ -60,32 +60,103 @@ $(function () {
             success: function (data) {
                 $(outputBlock).html('')
                 data.forEach(element => {
-                    var li = $('<li class="'+ liClass +'"></li>').text(element.title);
+                    var li = $('<li class="' + liClass + '"></li>').text(element.title);
                     li.attr('data-location', element.title);
                     li.attr('data-train', element.value);
-                    $(outputBlock).append(li);  
-                   
+                    $(outputBlock).append(li);
+
                 });
             }
         });
     }
 
-    getTrainsList = function(){
+    getTrainsList = function () {
         $.ajax({
             type: 'POST',
-            
+
             url: 'https://booking.uz.gov.ua/ru/train_search/',
 
             data: {
-                from : opt.fromValue,
-                to : opt.toValue,
-                date : opt.date,
-                time : opt.time 
+                from: opt.fromValue,
+                to: opt.toValue,
+                date: opt.date,
+                time: opt.time
             },
             success: function (data) {
-                console.log(data)
+                filterTrains(data);
+                // console.log(data)
             }
         });
+    }
+
+    filterTrains = function (trainsArr) {
+
+        trainsArr.data.list.forEach(function (element) {
+            if (element.types.length > 0) {
+                pushTrainsToEx(element)
+            }
+        })
+        initTrainsList()
+
+    }
+
+    pushTrainsToEx = function (trainObj) {
+        console.log(trainObj)
+        var content = '';
+        trainObj.types.forEach(function (element) {
+            //&wagon_num=33&url=train-wagons
+            content = '<div class="trains__list" data-trainNum=' + trainObj.num +
+                ' data-trainid=' + element.id + '> \
+            <p>'+ element.title + ': <span> ' + element.places + ' </a><span></p>\
+        </div>'
+
+        })
+        $('.js_trainsList--container').append(content)
+
+    }
+
+    initTrainsList = function () {
+        console.log('was inited')
+        $('.trains__list').on('click', function () {
+            openThisTrain($(this).attr('data-trainNum'), $(this).attr('data-trainId'))
+        })
+    }
+
+    openThisTrain = function (trainNum, trainId) {
+        $.ajax({
+            type: 'POST',
+
+            url: 'https://booking.uz.gov.ua/ru/train_wagons/',
+
+            data: {
+                from: opt.fromValue,
+                to: opt.toValue,
+                date: opt.date,
+                train: trainNum,
+                wagon_type_id: trainId
+            },
+            success: function (data) {
+
+                console.log(data.data.wagons[0].num)
+                redirectToUrl(data.data.wagons[0].num, trainNum, trainId)
+            }
+        });
+    }
+
+    redirectToUrl = function (wagons, trainNum, trainId) {
+
+        
+        chrome.tabs.create({
+            url: "https://booking.uz.gov.ua/ru/?from=" + opt.fromValue +
+                "&to=" + opt.toValue +
+                "&date=" + opt.date +
+                "&time=" + opt.time +
+                "&train=" + trainNum +
+                "&wagon_type_id=" + trainId +
+                "&wagon_num=" + wagons +
+                "&url=train-wagons"
+        });
+
     }
 
 })
